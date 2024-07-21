c62572ac0b607b9ea90c73bcf2e862b6057bb6f2
promowar <56930854+promowar@users.noreply.github.com>
2024-07-21 16:04:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+course_name = 'Python'
+time_for_one_task = (hours/homework)
+print ('Курс:',course_name,', всего задач:',homework,', затрачено часов:', hours,', среднее время выполнения', time_for_one_task, 'часа')
